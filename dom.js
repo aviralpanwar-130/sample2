7c106d738001aeb6acb09af2b37d32eac5f128f7
aviralpanwar-130 <aviralpanwar263@gmail.com>
2023-03-20 17:13:27 +0530

    update

--- dom.js
+++ dom.js
@@ -13,7 +13,7 @@
 //console.log(document.links);
 //console.log(document.images);
 
-// Get element by ID.
+// Get element by ID.....
 
 //console.log(document.getElementById('header-title'));
 let headerTitle = document.getElementById('header-title');
@@ -23,9 +23,57 @@ let headerTitle = document.getElementById('header-title');
 //headerTitle.innerHTML = '<h3>Good Job</h3>';
 headerTitle.style.border='solid 2px #000';
 
-//Get elemenst by class.
+//Get elemenst by class......
 
-let addList = document.getElementsByClassName('title')[0];
-console.log(addList);
-addList.style.color='green';
-addList.style.fontWeight='Bold';
+// let addList = document.getElementsByClassName('title')[0];
+// console.log(addList);
+// addList.style.color='green';
+// addList.style.fontWeight='Bold';
+// let items = document.getElementsByClassName('list-group-item');
+// for(let i=0;i<items.length;i++)
+// {
+//     items[i].style.backgroundColor='grey';
+// }
+
+//Get elemenst by Tag Name......
+
+
+// let items = document.getElementsByTagName('li');
+// for(let i=0;i<items.length;i++)
+// {
+//     items[i].style.backgroundColor='grey';
+// }
+
+//Query Selector.......
+
+// let header = document.querySelector('#main-header');
+// header.style.borderColor='solid 4px #ccc';
+
+// let input = document.querySelector('input');
+// input.value='Hello User';
+
+// let submit = document.querySelector('input[type="submit"]');
+// submit.value='SEND';
+
+// let item = document.querySelector('.list-group-item');
+// item.style.color='red';
+
+// let lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='blue';
+
+// let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color='green';
+
+
+// QuerySelectorAll.....
+
+let titles = document.querySelectorAll('.title');
+titles[0].textContent='Hello';
+
+let odd = document.querySelectorAll('li:nth-child(odd)');
+let even = document.querySelectorAll('li:nth-child(even')
+for(let i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='grey';
+    even[i].style.backgroundColor='yellow';
+}
